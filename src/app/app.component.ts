41e102fb90954540f162542def3859ea467e57b4
JaviGarrido22 <jav.garridom@duocuc.cl>
2024-09-07 21:46:57 -0400

    Diseño app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,23 @@ import { Component } from '@angular/core';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
+
+
 export class AppComponent {
-  constructor() {}
+  public progress = 0;
+
+  constructor() {
+    setInterval(() => {
+      this.progress += 0.01;
+
+      // Reset the progress bar when it reaches 100%
+      // to continuously show the demo
+      if (this.progress > 1) {
+        setTimeout(() => {
+          this.progress = 0;
+        }, 1000);
+      }
+    }, 50);
+  }
 }
+
