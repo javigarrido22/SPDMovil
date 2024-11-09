e2dee2e88d076a74597cfa937b712cd16ab5ba70
Javigarrido22 <jav.garridom@duocuc.cl>
2024-11-09 14:05:23 -0300

    test

--- src/app/page/principal/principal.page.spec.ts
+++ src/app/page/principal/principal.page.spec.ts
@@ -3,14 +3,18 @@ import { PrincipalPage } from './principal.page';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from 'src/environments/environment';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('PrincipalPage', () => {
   let component: PrincipalPage;
   let fixture: ComponentFixture<PrincipalPage>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports:[AngularFireAuthModule, AngularFireModule.initializeApp(environment.firebaseConfig)]
+  beforeEach(async() => {
+    await TestBed.configureTestingModule({
+      imports:[AngularFireAuthModule, AngularFireModule.initializeApp(environment.firebaseConfig)],
+      providers:[provideHttpClient,{provide:ActivatedRoute, useValue:{queryParams:of({email:'email@gmail.com'})}}]
     })
     fixture = TestBed.createComponent(PrincipalPage);
     component = fixture.componentInstance;
