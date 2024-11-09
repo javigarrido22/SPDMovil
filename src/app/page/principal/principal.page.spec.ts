d227bd939b021118086a28fda42401b679844333
Javigarrido22 <jav.garridom@duocuc.cl>
2024-11-09 13:30:50 -0300

    test

--- src/app/page/principal/principal.page.spec.ts
+++ src/app/page/principal/principal.page.spec.ts
@@ -1,11 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PrincipalPage } from './principal.page';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from 'src/environments/environment';
 
 describe('PrincipalPage', () => {
   let component: PrincipalPage;
   let fixture: ComponentFixture<PrincipalPage>;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports:[AngularFireAuthModule, AngularFireModule.initializeApp(environment.firebaseConfig)]
+    })
     fixture = TestBed.createComponent(PrincipalPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
